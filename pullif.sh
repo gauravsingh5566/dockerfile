ce51a3b05c4d21c30bbc82e9ae4aa84019cf912f
darkomni5566 <100135137+darkomni5566@users.noreply.github.com>
2022-07-08 20:04:45 +0530

    Create pullif.sh

---
+++ pullif.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+changed=0
+git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
+if [ $changed = 1 ]; then
+    git pull
+    npm install
+    npm run build
+    mv dist /var/www/html/
+    echo "Updated successfully";
+else
+    echo "Up-to-date"
+fi
