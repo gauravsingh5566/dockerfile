236c3a43e694c2de21b68a8bc636aafe0f988718
darkomni5566 <100135137+darkomni5566@users.noreply.github.com>
2022-07-08 20:03:53 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-cd angular-starter-master
+ git clone https://github.com/darkomni5566/angular-starter-master.git
+cd angular-starter-master/
+npm install
+npm run build
+mv dist/ /var/www/html/
 
-changed=0
-git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
-if [ $changed = 1 ]; then
-    git pull
-    npm install
-    npm run build
-    mv dist /var/www/html/
-    echo "Updated successfully";
-else
-    echo "Up-to-date"
-fi
+#changed=0
+#git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
+#if [ $changed = 1 ]; then
+#    git pull
+#    npm install
+#    npm run build
+#    mv dist /var/www/html/
+#    echo "Updated successfully";
+#else
+#    echo "Up-to-date"
+#fi
+
+
+
+
